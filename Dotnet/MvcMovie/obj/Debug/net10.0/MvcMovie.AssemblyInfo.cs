0254a2500f561d28a0b35a504f762d0174052714
brzosm-admin <brzosm5118690_aehit@students.vizja.pl>
2026-05-09 10:39:06 +0200

    work with a database

--- Dotnet/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ Dotnet/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fba7c73411e6b9b845384f66f790a5a136ad0b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e84df0eb0c46713a36128a1c22cfc607e35d877")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
