1daf2271153df9a907c795668cdd6f7199209f57
brzosm-admin <brzosm5118690_aehit@students.vizja.pl>
2026-05-09 20:16:48 +0200

    add validation

--- Dotnet/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ Dotnet/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ef7ba2e784d68f39c4958d206824da8281ed9db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fd7392631a1c784eadcccdb19d27b8acd896a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
