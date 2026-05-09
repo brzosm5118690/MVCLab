b7b6103ecb86be38de603dc1e611d89fafc4ad20
brzosm-admin <brzosm5118690_aehit@students.vizja.pl>
2026-05-09 15:39:49 +0200

    controller actions and views

--- Dotnet/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ Dotnet/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e84df0eb0c46713a36128a1c22cfc607e35d877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0254a2500f561d28a0b35a504f762d0174052714")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
